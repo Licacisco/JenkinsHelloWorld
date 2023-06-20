71635cedfbe00ff90645c10f50a63d0c11060c5d
Kenneth Ampim <kwabenaampim@yahoo.com>
2023-06-20 12:07:43 +0200

    Add new files

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+import java.lang.*;
+
+public class HelloWorld {
+
+	public static void main(String[] args){
+		System.out.println("--------****Hello Hello world and Welcome to Jenkins*****-----");
+
+		int count;
+		count = 10;
+		for(int i=0; i<=count; i++)
+			System.out.println("Number: " +i + " is DONE");
+	}
+}
